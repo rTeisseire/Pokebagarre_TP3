17ac6b8f5a263c184151d3e1e574b238216c8ff0
rTeisseire <teisseireraphael@gmail.com>
2026-02-24 08:36:10 +0100

    Commit d'initialisation

---
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -0,0 +1,20 @@
+/**
+ * Classe de test pour la méthode estVainqueurContre de la classe Pokemon.
+ * * Liste des tests :
+ * - Le premier Pokémon gagne s'il a une meilleure attaque.
+ * - Le second Pokémon gagne s'il a une meilleure attaque.
+ * - Le premier Pokémon gagne s'il a la même attaque mais une meilleure défense.
+ * - Le second Pokémon gagne s'il a la même attaque mais une meilleure défense.
+ * - Le premier Pokémon gagne par défaut s'ils ont les mêmes statistiques.
+ */
+
+package com.montaury.pokebagarre.metier;
+
+import com.montaury.pokebagarre.fixtures.ConstructeurDePokemon;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PokemonTest {
+
+}
